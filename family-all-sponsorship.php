a53091500a5491a81aef2ba9d99df997f772b5ba
Vidhya Jayakumar <vidhya@Vidyas-MacBook-Pro.local>
2020-05-03 18:39:07 -0400

    Changed the pics of the files

--- family-all-sponsorship.php
+++ family-all-sponsorship.php
@@ -72,7 +72,7 @@
                 <div class="content-column col-lg-9 col-md-8 col-sm-12">
                 	<div class="inner-column">
                         <div class="image">
-                        	<img src="images/resource/company-4.jpg" alt="" />
+                        	<img src="images/resource/family-all-sponsorship.jpg" alt="" />
                         </div>
                         <h2>Canada - Family Sponsorship</h2>
                         <div class="text">
